e82ef42993e66528b5b3fda340122548725be187
1mrasts <olgaakshidi.ru@gmail.com>
2026-01-27 22:51:00 +0300

    SCRUM-9: create a function to delete a price add a new component for future settings

--- src/utils/dateAction.ts
+++ src/utils/dateAction.ts
@@ -1,6 +1,22 @@
+import type { Dispatch, SetStateAction } from 'react'
+import type { datesType } from '../components/Calendar/CalendarDays/CalendarDays.types'
+
 export function handleDateClick(
-	e: React.MouseEvent<HTMLSpanElement>,
 	id: number,
+	days: datesType[], // Типизируем state
+	setDays: Dispatch<SetStateAction<datesType[]>>, // Типизируем setState
 ) {
-	console.log(e.currentTarget.textContent, ' | ', id)
+	if (days.find(item => item.id == id)?.meta != undefined) {
+		// Выставляем price
+		setDays(
+			days.map(item => (item.id === id ? { ...item, meta: undefined } : item)),
+		)
+	} else {
+		// Убираем price
+		setDays(
+			days.map(item =>
+				item.id === id ? { ...item, meta: { price: 2200 } } : item,
+			),
+		)
+	}
 }
