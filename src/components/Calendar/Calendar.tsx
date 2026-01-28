c51f065b584b450efa2fd18ce221ca0cd5bfb797
1mrasts <olgaakshidi.ru@gmail.com>
2026-01-28 22:54:33 +0300

    SCRUM-10: move state to Calendar component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react'
 import { CalendarDays } from './CalendarDays/CalendarDays'
+import type { datesType } from './CalendarDays/CalendarDays.types'
 import { CalendarHeader } from './CalendarHeader/CalendarHeader'
+import { CalendarInfo } from './CalendarInfo/CalendarInfo'
 
 export function Calendar() {
+	const [days, setDays] = useState<datesType[]>([])
 	return (
 		<main>
 			<CalendarHeader />
-			<CalendarDays />
+			<CalendarDays days={days} setDays={setDays} />
+			<CalendarInfo days={days} />
 		</main>
 	)
 }
