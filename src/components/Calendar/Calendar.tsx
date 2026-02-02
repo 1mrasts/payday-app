82c975abfa9f5caef6899e590cec2958adaa4729
1mrasts <olgaakshidi.ru@gmail.com>
2026-02-02 22:59:05 +0300

    SCRUM-11: add the new states for "time" and "currentDay" options refactor the structure of the Calendar component

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,17 +1,39 @@
 import { useState } from 'react'
+import styles from './Calendar.module.css'
+import { CalendarDay } from './CalendarDay/CalendarDay'
 import { CalendarDays } from './CalendarDays/CalendarDays'
-import type { datesType } from './CalendarDays/CalendarDays.types'
+import type { datesType, time } from './CalendarDays/CalendarDays.types'
 import { CalendarHeader } from './CalendarHeader/CalendarHeader'
 import { CalendarInfo } from './CalendarInfo/CalendarInfo'
 import { Settings } from './Settings/Settings'
 
 export function Calendar() {
 	const [days, setDays] = useState<datesType[]>([])
+	const [currentDay, setCurrentDay] = useState<datesType | undefined>(undefined)
+	const [time, setTime] = useState<time>()
 	const [salary, setSalary] = useState<number>(141)
 	return (
 		<main>
-			<CalendarHeader />
-			<CalendarDays days={days} setDays={setDays} salary={salary} />
+			<div className={styles.calendar}>
+				<div className={styles['calendar__main']}>
+					<CalendarHeader />
+					<CalendarDays
+						days={days}
+						setDays={setDays}
+						salary={salary}
+						setCurrentDay={setCurrentDay}
+					/>
+				</div>
+				<div>
+					<CalendarDay
+						day={currentDay}
+						setTime={setTime}
+						setDays={setDays}
+						time={time}
+						days={days}
+					/>
+				</div>
+			</div>
 			<CalendarInfo days={days} />
 			<Settings setSalary={setSalary} />
 		</main>
