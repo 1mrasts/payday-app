91a0cdf07870f7cf04d1490a1833c28a10e50b13
1mrasts <olgaakshidi.ru@gmail.com>
2026-02-03 21:11:04 +0300

    SCRUM-12: Fix a bug and add new features Display the start time of the day Display the end time of the day

--- src/components/Calendar/Settings/Settings.tsx
+++ src/components/Calendar/Settings/Settings.tsx
@@ -1,12 +1,27 @@
-import type { Dispatch, SetStateAction } from 'react'
+import { useState, type Dispatch, type SetStateAction } from 'react'
+import type { time } from '../CalendarDays/CalendarDays.types'
 
 export function Settings({
 	setSalary,
+	salary,
+	setTime,
 }: {
 	setSalary: Dispatch<SetStateAction<number>>
+	salary: number
+	setTime: Dispatch<SetStateAction<time | undefined>>
 }) {
+	const [startTime, setStartTime] = useState<string>('')
+	const [endTime, setEndTime] = useState<string>('')
+
+	function saveTime() {
+		setTime({
+			startTime: startTime,
+			finishTime: endTime,
+		})
+	}
 	return (
 		<>
+			<span>Почасовая ставка: </span>
 			<input
 				type='number'
 				onChange={e =>
@@ -14,7 +29,16 @@ export function Settings({
 						? setSalary(0)
 						: setSalary(Number(e.target.value))
 				}
+				value={salary}
 			/>
+			<br />
+			<span>Начало дня:</span>
+			<input type='time' onChange={e => setStartTime(e.target.value)} />
+			<br />
+			<span>Конец дня:</span>
+			<input type='time' onChange={e => setEndTime(e.target.value)} />
+			<br />
+			<button onClick={saveTime}>Подтвердить</button>
 		</>
 	)
 }
